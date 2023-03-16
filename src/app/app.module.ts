7e812de9380044cb85e577d27383aff165cd346f
cmak34 <cmak34@my.centennialcollege.ca>
2023-03-16 11:37:12 -0400

    update the app module to import certain module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,77 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Ng Zorro
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzMentionModule } from 'ng-zorro-antd/mention';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzAffixModule } from 'ng-zorro-antd/affix';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzTreeModule } from 'ng-zorro-antd/tree';
+import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+
+// Angular
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
+
+// Firebase
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+
+// Custom Components
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -45,9 +104,61 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    NzLayoutModule,
+    NzModalModule,
+    NzPopconfirmModule,
+    NzDrawerModule,
+    NzDividerModule,
+    NzButtonModule,
+    NzNotificationModule,
+    NzGridModule,
+    NzInputModule,
+    NzIconModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzToolTipModule,
+    NzCardModule,
+    NzAvatarModule,
+    NzUploadModule,
+    NzDescriptionsModule,
+    NzTypographyModule,
+    NzDropDownModule,
+    NzCheckboxModule,
+    NzDatePickerModule,
+    NzFormModule,
+    NzInputNumberModule,
+    NzMentionModule,
+    NzRadioModule,
+    NzRateModule,
+    NzSelectModule,
+    NzSliderModule,
+    NzSwitchModule,
+    NzTimePickerModule,
+    NzBadgeModule,
+    NzAlertModule,
+    NzCalendarModule,
+    NzCarouselModule,
+    NzCollapseModule,
+    NzEmptyModule,
+    NzImageModule,
+    NzResultModule,
+    NzListModule,
+    NzPopoverModule,
+    NzStatisticModule,
+    NzAffixModule,
+    NzTableModule,
+    NzTabsModule,
+    NzTagModule,
+    NzTimelineModule,
+    NzTreeModule,
+    NzTreeViewModule,
+    NzStepsModule,
+    NzSpaceModule,
+    NzProgressModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
