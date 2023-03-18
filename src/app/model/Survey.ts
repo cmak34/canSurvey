fd25b7dcb26fa5c06212e5635327a1bfb4a19147
cmak34 <cmak34@my.centennialcollege.ca>
2023-03-18 18:08:23 -0400

    changed survey model

--- src/app/model/Survey.ts
+++ src/app/model/Survey.ts
@@ -3,16 +3,12 @@ export interface Survey {
     id: string
     title: string
     desc: string
-    createdTime: firebase.default.firestore.Timestamp
-    updatedTime: firebase.default.firestore.Timestamp
-}
-
-export interface Question {
-    id: string
-    label: string
-    type: ("text"|"textarea"|"select"|"radio"|"checkbox"|"date")
-    options: any[]
-    required: boolean
+    questions: {
+        label: string
+        type: ("text"|"textarea"|"select"|"radio"|"checkbox"|"date")
+        options: any[]
+        required: boolean
+    }[]
     createdTime: firebase.default.firestore.Timestamp
     updatedTime: firebase.default.firestore.Timestamp
 }
