7543c146c3599e626d2c32ac25f7e4e059642f51
cmak34 <cmak34@my.centennialcollege.ca>
2023-03-18 17:52:52 -0400

    changed login modal && added the login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzModalRef } from 'ng-zorro-antd/modal';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit  {
 
+  public isLoading = false;
+  public form?: FormGroup 
+
+  constructor(
+    private auth: AuthService,
+    private fb: FormBuilder,
+    private modalRef: NzModalRef,
+    private notification: NzNotificationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: this.fb.control(null, [Validators.required, Validators.email]),
+      password: this.fb.control(null, [Validators.required, Validators.minLength(8), Validators.maxLength(8)]),
+    })
+  }
+
+  public async submit() {
+    for (const i in this.form?.controls) {
+      this.form?.controls[i]?.markAsDirty();
+      this.form?.controls[i]?.updateValueAndValidity();
+    }
+    if (this.form?.valid && !this.isLoading) { 
+      this.isLoading = true
+      try {
+        await this.auth.login(this.form.value.email, this.form.value.password)
+        this.isLoading = false
+        await this.modalRef.close(true)
+      } catch (error) {
+        console.log(error)
+        this.isLoading = false
+        this.notification.error('Error', `There was an error logging in: ${error}`)
+      }
+    } else {
+      this.isLoading = false
+      this.notification.error('Error', 'Please fill out the form correctly')
+    } 
+  }
+
+  public async cancel() {
+    await this.modalRef.close(false)
+  }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
-import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +9,6 @@ export class AuthService {
   public user: (firebase.User | null) = null
 
   constructor(
-    
     private afAuth: AngularFireAuth
   ) { 
     this.getAuth()
@@ -20,6 +18,10 @@ export class AuthService {
     this.afAuth.onAuthStateChanged(async (user) => {
       this.user = user;
     })
+  }
+
+  public async login(email: string, password: string) {
+    await this.afAuth.signInWithEmailAndPassword(email, password)
   }
 
   public async logout() {
