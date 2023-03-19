4f0427c96e08249f74d6eca13326794b79088f41
cmak34 <cmak34@my.centennialcollege.ca>
2023-03-18 20:43:58 -0400

    update the home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Survey } from 'src/app/model/Survey';
+import {catchError, map, Observable, of} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
+import {NzNotificationService} from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,20 @@ import { Survey } from 'src/app/model/Survey';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent {
-  public surveys: Survey[] = []
+  public surveys$: Observable<Survey[]> = this.afs.collection<Survey>("surveys", ref => ref.orderBy("createdTime", "desc").where("isPublished", "==", true))
+  .snapshotChanges()
+  .pipe(
+    map(actions => actions.map(action => ({ ...action.payload.doc.data() as any, id: action.payload.doc.id }))),
+    catchError((error) => {
+      console.error("Error getting surveys:", error);
+      this.notification.error("Error", `Error getting surveys: ${error}`);
+      return of([]);
+    })
+  )
+
+  constructor(
+    private notification: NzNotificationService,
+    private afs: AngularFirestore,
+  ) { }
 
 }
--- src/app/pages/survey/survey.component.ts
+++ src/app/pages/survey/survey.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { filter, map } from 'rxjs';
+import {Survey} from 'src/app/model/Survey';
 
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.less']
 })
-export class SurveyComponent {
+export class SurveyComponent implements OnInit {
+  public isLoading: boolean = false
+  public survey?: Survey
+  constructor(
+    private notification: NzNotificationService,
+    private route: ActivatedRoute,
+    private afs: AngularFirestore
+  ) { }
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.loadSurvey(params['id'] || "");
+    });
+  }
+
+  public async loadSurvey(surveyId: string) {
+    if (surveyId !== "") {
+      try {
+        this.isLoading = true;
+        this.survey = await this.afs.collection<Survey>("surveys").doc(surveyId).get().pipe(
+          filter((action) => action.exists),
+          map((action) => ({...action.data() as any, id: action.id}) as Survey),
+        ).toPromise();
+      } catch (error) {
+        console.error(`Error getting survey ${surveyId}:`, error);
+        this.notification.error("Error", `Error getting survey ${surveyId}: ${error}`)
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  }
 }
