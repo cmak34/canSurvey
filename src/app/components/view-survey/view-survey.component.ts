3df8a021688dcb9e74ff1b82ed51b1ec099f21b7
chinnawutb <chinnawut.b@pomelofashion.com>
2023-03-31 18:27:12 -0400

    feature: complete export csv file in survay Dashboard page

--- src/app/components/view-survey/view-survey.component.ts
+++ src/app/components/view-survey/view-survey.component.ts
@@ -41,4 +41,35 @@ export class ViewSurveyComponent implements OnInit {
   public close() {
     this.modalRef.close();
   }
+
+  public exportToCsv() {
+    if (this.survey?.id) {
+    this.results$.subscribe(results => {
+      // Generate header from survey questions
+      const header = this.survey?.questions.map(question => question.label).join(',') + '\n';
+  
+      // Format answers as CSV rows
+      const csvData = results.map(result => {
+        return result.answers.map(answer => {
+          if (typeof answer === 'object' && answer !== null) {
+            return JSON.stringify(answer);
+          }
+          return answer;
+        }).join(',');
+      }).join('\n');
+  
+      const csvContent = header + csvData;
+  
+      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+      const link = document.createElement("a");
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", `survey_results_${this.survey?.title}_${this.survey?.id}.csv`);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  }
+  }
 }
