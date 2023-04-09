569c9e1626a93d43c6b71c3e3bc56f7e4c5c1145
cmak34 <cmak34@my.centennialcollege.ca>
2023-04-08 22:50:00 -0400

    tidy up

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/model/User';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { catchError, map, Observable, of } from 'rxjs';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
-import { EditProfileComponent } from 'src/app/components/edit-profile/edit-profile.component';
-import { NzDateMode } from 'ng-zorro-antd/date-picker';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
 import { serverTimestamp } from 'firebase/firestore';
 
@@ -18,49 +13,49 @@ import { serverTimestamp } from 'firebase/firestore';
 export class UserProfileComponent implements OnInit {
   public isLoading = false;
   public form?: FormGroup;
+  public subscription: any;
 
+  constructor(
+    private afs: AngularFirestore,
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private notification: NzNotificationService
+  ) {
 
-    constructor(
-      private afs: AngularFirestore,
-      private fb: FormBuilder,
-      private auth: AuthService,
-      // private modalRef: NzModalRef,
-      private notification: NzNotificationService
-    ) {
-      
-    }
+  }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+    this.subscription = this.auth.profile$.subscribe(user => {
       this.form = this.fb.group({
-        userName: this.fb.control(this.auth.userProfile?.userName || null, [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
-        firstName: this.fb.control(this.auth.userProfile?.firstName || null, [Validators.required]),
-        lastName: this.fb.control(this.auth.userProfile?.lastName || null, [Validators.required]),
-        dob: this.fb.control(this.auth.userProfile?.dob?.toDate() || null, [Validators.required]),
-        province: this.fb.control(this.auth.userProfile?.province || null, [Validators.required])
+        userName: this.fb.control(user?.userName || null, [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+        firstName: this.fb.control(user?.firstName || null, [Validators.required]),
+        lastName: this.fb.control(user?.lastName || null, [Validators.required]),
+        dob: this.fb.control(user?.dob?.toDate() || null, [Validators.required]),
+        province: this.fb.control(user?.province || null, [Validators.required])
       })
-    }
+    })
+  }
 
-    public async submit() {
-      if (this.form?.valid && !this.isLoading && this.auth.user?.uid) {
-        this.isLoading = true;
-        try {
-          let result = await this.afs.collection("users").doc(this.auth.user?.uid).update({
-            userName: this.form?.value.userName?.trim(),
-            firstName: this.form?.value.firstName?.trim(),
-            lastName: this.form?.value.lastName?.trim(),
-            dob: this.form?.value.dob,
-            province: this.form?.value.province?.trim(),
-            updateTime: serverTimestamp()
-          }) 
-          this.notification.success("Success", "The user profile has been updated")
-          this.isLoading = false
-        } catch (error) {
-          console.log(error);
-          this.isLoading = false;
-          this.notification.error('Error', `There was an error: ${error}`);
-        }
+  public async submit() {
+    if (this.form?.valid && !this.isLoading && this.auth.user?.uid) {
+      this.isLoading = true;
+      try {
+        await this.afs.collection("users").doc(this.auth.user?.uid).update({
+          userName: this.form?.value.userName?.trim(),
+          firstName: this.form?.value.firstName?.trim(),
+          lastName: this.form?.value.lastName?.trim(),
+          dob: this.form?.value.dob,
+          province: this.form?.value.province?.trim(),
+          updateTime: serverTimestamp()
+        })
+        this.notification.success("Success", "The user profile has been updated")
+        this.isLoading = false
+      } catch (error) {
+        console.log(error);
+        this.isLoading = false;
+        this.notification.error('Error', `There was an error: ${error}`);
       }
     }
-    
+  }
 }
 
